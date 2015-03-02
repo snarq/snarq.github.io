92ead8a1c3882806c853c8f200aea09b79620b48
Ronis Nascimento <snarqie@gmail.com>
2015-03-02 00:14:48 -0300

    Update clean-blog.js

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -21,23 +21,23 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
+            var name = $("input#nome").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
+            var assunto = $("input#assunto").val();
+            var mensagem = $("textarea#mensagem").val();
+            var primeiroNome = nome; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
+            if (primeiroNome.indexOf(' ') >= 0) {
+                primeiroNome = nome.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://snarqie.herokuapp.com/mail/",
+                url: "https://snarqie.herokuapp.com/mail.php",
                 type: "POST",
                 data: {
-                    name: name,
-                    phone: phone,
+                    nome: nome,
+                    assunto: assunto,
                     email: email,
-                    message: message
+                    mensagem: mensagem
                 },
                 cache: false,
                 success: function() {
@@ -46,7 +46,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Sua mensagem foi enviada.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -58,7 +58,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Desculpe " + primeiroNome + ", parece que o meu servidor de email não está respondendo. Por favor, tente novamente mais tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
